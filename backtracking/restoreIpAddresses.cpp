fe3fba080092789d46a9ebf04ccf9de3f7299891
easonyipj <34618178+easonyipj@users.noreply.github.com>
2019-07-14 22:37:26 +0800

    update solutions

--- backtracking/restoreIpAddresses.cpp
+++ backtracking/restoreIpAddresses.cpp
@@ -13,7 +13,26 @@ namespace backtracking{
 
         }
 
-        static void findIp(string& s, int idx, int part, string& temp, vector<string> &res) {
+        static void findIp(string& s, int idx, int part, string temp, vector<string> &res) {
+            if(part == 4 && idx == s.length()) {
+                res.emplace_back(temp.substr(0, temp.length() - 1));
+            }
+
+            else if(part < 4) {
+                if(s[idx] == '0') {
+                    findIp(s, idx + 1, part + 1, temp + "0.", res);
+                    return;
+                }
+                for(int i = 1; i < 4; i++) {
+                    if(idx + i - 1 == s.length()) {
+                        return;
+                    }
+                    int address = stoi(s.substr(idx, i));
+                    if(address < 256) {
+                        findIp(s, idx + i, part + 1, temp + to_string(address) + ".", res);
+                    }
+                }
+            }
 
         }
 
